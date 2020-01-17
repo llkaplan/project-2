0f5fa983f154deb74cff325d774c24cdeccd7bd0
Jocelyn Chang <jocelync1094@gmail.com>
2020-01-16 19:02:50 -0800

    removed category and items table from data, also removing handlebar

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
-// Sets up the Express App
-// =============================================================
+
 var app = express();
 var PORT = process.env.PORT || 8080;
-var exphbs = require("express-handlebars");
 
 // Static directory
 app.use(express.static("public"));
@@ -20,7 +18,6 @@ app.use(express.json());
 
 // Set Handlebars.
 
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -28,7 +25,7 @@ app.set("view engine", "handlebars");
 // =============================================================
 // require("./routes/html-routes.js")(app);
 // require("./routes/author-api-routes.js")(app);
-// require("./routes/post-api-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
