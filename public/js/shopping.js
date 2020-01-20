06f8b5c9f5f688479ffeff30bcd2cfa3759bcd6f
Duc T <ductran963@gmail.com>
2020-01-19 16:44:15 -0800

    working on comments

--- public/js/shopping.js
+++ public/js/shopping.js
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-
-$(document).ready(function () {
-
-  $(".item").on("click", function () {
-    var itemPage = ($(this).attr("id"));
-    document.location.href = "/item/" + itemPage;
-  });
-
-  $("#submitBtn").on("click", function (event) {
-    event.preventDefault();
-    var newReview = {
-      name: $("#name").html(),
-      comment: $("#comment").html(),
-    };
-    console.log("this is taking in the onclick event");
-    console.log(newReview);
-
-
-
-
-
-
-
-
-    // // Send an AJAX POST-request with jQuery
-    // $.post("/api/new", newChirp)
-    //   // On success, run the following code
-    //   .then(function() {
-
-    //     var row = $("<div>");
-    //     row.addClass("chirp");
-
-    //     row.append("<p>" + newChirp.author + " chirped: </p>");
-    //     row.append("<p>" + newChirp.body + "</p>");
-    //     row.append("<p>At " + moment(newChirp.created_at).format("h:mma on dddd") + "</p>");
-
-    //     $("#chirp-area").prepend(row);
-
-    //   });
-
-    // Empty each input box by replacing the value with an empty string
-    // $("#name").val("");
-    // $("#comment").val("");
-  });
-
-
-
-});
-
-=======
 $(document).ready(function() {
   var itemPage;
 
@@ -114,4 +63,3 @@ $(document).ready(function() {
     }
   });
 });
->>>>>>> da34ee0e159e2081565683b7968e3d3b897f5686
