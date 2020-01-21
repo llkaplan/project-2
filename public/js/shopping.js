a6bd51ce41154d8c21da0408faf8dfc35e80cd75
Jocelyn Chang <jocelync1094@gmail.com>
2020-01-21 00:14:10 -0800

    completed calculation of order total and render to page

--- public/js/shopping.js
+++ public/js/shopping.js
@@ -1,3 +1,4 @@
+var total = [];
 $(document).ready(function() {
   var itemPage;
 
@@ -67,16 +68,13 @@ $(document).ready(function() {
 
     }
   });
-  var orderTotal;
   //adding item to cart table
   $(".add").on("click",function(event){
     event.preventDefault();
     var uneditedPrice = $(".item-price").text();
     var newPrice = uneditedPrice.replace("$","");
-    orderTotal = orderTotal + newPrice;
-  
-    console.log(newPrice);
 
+    console.log(newPrice);
     var ItemChosen = {
       name: $(".item-name").text(),
       //price: $(".item-price").text(),
@@ -93,29 +91,43 @@ $(document).ready(function() {
   });
 
   //making ajax call for tax rates
-  $(".cart").on("click",function(){
-    var city="sandiego";
-    var state="ca";
+  $("#submitInfo").on("click",function(){
+    var city=$("#city").val().trim();
+    var state=$("#state").val().trim();
     var queryURL ="https://trial.serviceobjects.com/FT/web.svc/json/GetTaxInfoByCityState?City="+city+"&State="+state+"&TaxType=sales&LicenseKey=WS19-QUN4-BKY4";
-
+    console.log(queryURL);
+    $(".price").each(function() {
+      var priceHtml = $(this).text();
+      var splitHtml = priceHtml.split("$");
+      var price = splitHtml[1];
+      console.log(price);
+      total.push(parseFloat(price));
+    });
+    console.log(total);
+    var totalsum=0;
+    for(var i =0;i<total.length;i++){
+      totalsum=totalsum+total[i];
+    }
+    console.log(totalsum);
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response){
 
-      var beforeTax = orderTotal;
-      var tax = response.Results[0].TotalTaxRate;
-      ordertotal = ordertotal * (1+tax);
+      var beforeTax = totalsum;
+      var tax = response.Results[0].TotalTaxRate.toFixed(2);
+      console.log(tax);
+      var ordertotal = ((totalsum*tax)+totalsum).toFixed(2);
       var orderDiv =$("<div>");
-      var orderP =$("<p> Order total before tax: " + beforeTax + "</p>");
-      var taxP = $("<p> Tax: " + tax + "</p>");
-      var total = $("<p> Order after tax: " + orderTotal + "</p>");
+      var orderP =$("<h5> Total before tax: $" + beforeTax + "</h5>");
+      var taxP = $("<h5> Estimated Tax to be collected: $" + tax + "</h5>");
+      var totalOrder = $("<h5> Order total after tax: $" + ordertotal + "</h5>");
 
       orderDiv.append(orderP);
       orderDiv.append(taxP);
-      orderDiv.append(total);
+      orderDiv.append(totalOrder);
 
-      $(".order-container").append(orderDiv);
+      $(".order-summary").append(orderDiv);
 
     });
 
